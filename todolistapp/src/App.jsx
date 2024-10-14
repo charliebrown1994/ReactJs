fa4090cb5037acff1f64b86cfb1c2cba7107f0ef
charliebrown1994 <leesungyul@outlook.com>
2024-10-14 21:48:56 +0900

    four commit

--- todolistapp/src/App.jsx
+++ todolistapp/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
@@ -16,7 +16,29 @@ const Wrapper = styled.div`
   margin: 100px auto 0;
 `;
 
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래하기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 const App = () => {
+  const [todo, setTodo] = useState([mockTodo]);
   return (
     <Wrapper>
       <Header />
